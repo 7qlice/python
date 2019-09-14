ef47aae3b4bcf789bc34ce8b0beada77d7db94a2
7qlice <55306909+7qlice@users.noreply.github.com>
2019-09-14 15:39:30 +0800

    Add files via upload

---
+++ 列表和数组的相互转换.py
@@ -0,0 +1,14 @@
+""""
+Author: Mr. Luo
+Date: 2019-09-14
+"""
+import numpy as np
+x = [1,2,3]
+print(x[:2])
+print("*"*9)
+y =np.array(x)
+# print(y)
+print(y[:2])
+print("*"*9)
+z = y.tolist()
+print(z)
